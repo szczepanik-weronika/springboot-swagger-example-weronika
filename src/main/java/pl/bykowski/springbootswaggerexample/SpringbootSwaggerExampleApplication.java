66d8854661423d2ee1681e4674a376c3fec85809
Jozef Piecyk <jozollo@gmail.com>
2021-06-11 22:36:48 +0200

    Initial commit

---
+++ src/main/java/pl/bykowski/springbootswaggerexample/SpringbootSwaggerExampleApplication.java
@@ -0,0 +1,17 @@
+package pl.bykowski.springbootswaggerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class SpringbootSwaggerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSwaggerExampleApplication.class, args);
+    }
+
+
+
+}
---
+++ src/main/java/pl/bykowski/springbootswaggerexample/Student.java
@@ -0,0 +1,47 @@
+package pl.bykowski.springbootswaggerexample;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
+
+@ApiModel("Personal data of Student")
+public class Student {
+
+    @ApiModelProperty("unique id of student")
+    private int id;
+    private String name;
+    private String surname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+}
