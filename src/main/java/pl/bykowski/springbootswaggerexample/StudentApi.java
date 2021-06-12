734de8df8189c3dfdab80cd8a8d8ee0ea1db73d9
Jozef Piecyk <jozollo@gmail.com>
2021-06-12 20:41:50 +0200

    change student url

--- src/main/java/pl/bykowski/springbootswaggerexample/StudentApi.java
+++ src/main/java/pl/bykowski/springbootswaggerexample/StudentApi.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/students")
+@RequestMapping("/students")
 public class StudentApi {
 
     private List<Student> studentList;
